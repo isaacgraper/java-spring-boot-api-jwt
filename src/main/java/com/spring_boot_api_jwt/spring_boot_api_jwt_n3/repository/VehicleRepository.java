c047255c46f078cd44079ebda0f14ca2c9fd0dab
Isaac Kleimmann Graper <121071941+isaacgraper@users.noreply.github.com>
2024-06-25 11:29:43 -0300

    Update VehicleRepository.java

--- src/main/java/com/spring_boot_api_jwt/spring_boot_api_jwt_n3/repository/VehicleRepository.java
+++ src/main/java/com/spring_boot_api_jwt/spring_boot_api_jwt_n3/repository/VehicleRepository.java
@@ -9,5 +9,4 @@ import java.util.Optional;
 @Repository
 public interface VehicleRepository extends JpaRepository<Vehicle, Long> {
     Optional<Vehicle> findByPlate(String plate);
-    Optional<Vehicle> deleteByPlate(String plate);
 }
