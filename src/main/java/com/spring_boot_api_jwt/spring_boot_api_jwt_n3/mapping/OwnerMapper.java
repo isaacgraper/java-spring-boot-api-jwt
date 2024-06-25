b8b918ff671064341e996c4f5e7ff7ca0e5450a6
Isaac Kleimmann Graper <121071941+isaacgraper@users.noreply.github.com>
2024-06-25 11:17:35 -0300

    Update OwnerMapper.java

--- src/main/java/com/spring_boot_api_jwt/spring_boot_api_jwt_n3/mapping/OwnerMapper.java
+++ src/main/java/com/spring_boot_api_jwt/spring_boot_api_jwt_n3/mapping/OwnerMapper.java
@@ -5,6 +5,35 @@ import com.spring_boot_api_jwt.spring_boot_api_jwt_n3.entity.Owner;
 
 public class OwnerMapper {
 
+    package com.spring_boot_api_jwt.spring_boot_api_jwt_n3.mapping;
+
+import com.spring_boot_api_jwt.spring_boot_api_jwt_n3.dto.OwnerDTO;
+import com.spring_boot_api_jwt.spring_boot_api_jwt_n3.entity.Owner;
+
+public class OwnerMapper {
+
+    // forma simplificada e melhor, porém requer mudança na estrutura do projeto
+    //  public OwnerDTO toDto(Owner owner) {
+    //     return new OwnerDTO(
+    //         owner.getId(),
+    //         owner.getCpf(),
+    //         owner.getPassword(), // remover o pwd por password, mudar entidade e dto
+    //         owner.getFirstName(),
+    //         owner.getPhone()
+    //     );
+    //   }
+    
+    //   public Owner toEntity(OwnerDTO ownerDTO) {
+    //     return new Owner(
+    //         ownerDTO.getId(),
+    //         ownerDTO.getCpf(),
+    //         ownerDTO.getPassword(),
+    //         ownerDTO.getFirstName(),
+    //         ownerDTO.getPhone()
+    //     );
+    //   }
+    // }
+    
     public static OwnerDTO mapToOwnerDTO(Owner owner) {
         return new OwnerDTO(
                 owner.getId(),
